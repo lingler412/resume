cfed846f836d47d5c758ef2cc5412052ab313467
lingler412 <roblingle2@gmail.com>
2025-02-09 18:48:17 -0500

    Fixed print and download buttons and margins

--- js/index.js
+++ js/index.js
@@ -1,5 +1,27 @@
-const downloadBtn = document.querySelector('.download-btn');
+const htmlContent = document.body;
 
-downloadBtn.addEventListener('click', () => {
+function printContent() {
     window.print();
-});
+}   
+
+function downloadContent() {
+const element = document.getElementById('document');
+if (element) {
+	const options = {
+	    filename: 'Rob_Lingle_Resume.pdf',   
+		margin: 0.5,   
+		image: { type: 'jpeg', quality: 0.98 },
+		html2canvas: { scale: 2 },
+		pagebreak: { avoid: 'li'},
+		jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+	};
+
+	if (typeof html2pdf !== 'undefined') {
+		html2pdf().set(options).from(element).save();
+	} else {
+		console.error("html2pdf library is not loaded.");
+	}
+} else {
+	console.error("Element with ID 'document' not found.");
+}
+}
